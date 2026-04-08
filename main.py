41c69ede0c46c4467ce8994a671297a9fb220f5b
JBoT21 <bowen.jackson@yahoo.com>
2026-04-08 09:38:21 -0400

    missed an import statement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import time
 
 # All import modules
-from hardware  import car, sonar, move, stop, get_distance
+from driver  import car, sonar, move, stop, get_distance
 from OccupancyGrid      import grid, update_grid, GRID_SIZE
 from AStar     import astar
 from QLearn import Q, choose_action, update_q, get_state, get_reward, ACTIONS
